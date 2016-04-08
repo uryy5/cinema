23efb3a79109b595a2d03f253fbe9dc90653fbb7
oriolqa <uryy_5@hotmail.com>
2016-04-08 22:09:26 +0200

    Add logout url.

--- cinema/urls.py
+++ cinema/urls.py
@@ -29,6 +29,6 @@ urlpatterns = patterns('',
     url(r'^$', mainpage, name='home'),
     url(r'^login/$', 'django.contrib.auth.views.login'),
     url(r'^icinema/', include('icinema.urls', namespace='icinema')),
-    # url(r'^api/cinema.json/$', cinemajson),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout', kwargs={'next_page': '/'}),
     url(r'^admin/', include(admin.site.urls)),
 )
