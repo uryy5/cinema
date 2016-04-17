2f2594119128db0028a6182a991a314fff37cff2
oriolqa <uryy_5@hotmail.com>
2016-04-17 14:44:09 +0200

    Update Performances for FilmsPerformances for the class models.py

--- icinema/admin.py
+++ icinema/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 import models
 # Register your models here.
 admin.site.register(models.Cinema)
-admin.site.register(models.Performances)
 admin.site.register(models.Films)
+admin.site.register(models.FilmsPerfomances)
