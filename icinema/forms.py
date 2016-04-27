f51b926007c5d9e0322ed71772a574056b842255
Ivan <ivanpereraq@gmail.com>
2016-04-27 12:26:10 +0200

    create FilmForm for edit films details

--- icinema/forms.py
+++ icinema/forms.py
@@ -1,8 +1,13 @@
 from django.forms import ModelForm
-from models import Cinema
+from models import Cinema, Films
 
 class CinemaForm(ModelForm):
     class Meta:
         model = Cinema
         exclude = ('name',)
+
+class FilmForm(ModelForm):
+    class Meta:
+        model = Films
+        exclude = ('user', 'cinema', 'tittle',)
 
