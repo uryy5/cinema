3ed164337f29746f5f6753b32132b1595f34c2cc
oriolqa <uryy_5@hotmail.com>
2016-05-14 22:22:21 +0200

    Modified urls

--- cinema/urls.py
+++ cinema/urls.py
@@ -15,21 +15,22 @@ Including another URLconf
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
 admin.autodiscover()
-from rest_framework.urlpatterns import format_suffix_patterns
-from views import APIFilmDetail, APIFilmList, APICinemaDetail, APICinemaList, APIPerformancesDetail, APIPerformancesList
 
-urlpatterns = [
+from icinema.views import *
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'sobres.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
 
-    #RESTful API
-    url(r'^api/auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^api/cinema/$', APICinemaList.as_view(), name='cinema-list'),
-    url(r'^api/cinema/(?P<pk>\d+)/$', APICinemaDetail.as_view(), name='cinema-detail'),
-    url(r'^api/films/$', login_required(APIFilmList.as_view()), name='film-list'),
-    url(r'^api/films/(?P<pk>\d+)/$', APIFilmDetail.as_view(), name='film-detail'),
-    url(r'^api/performances/$', APIPerformancesList.as_view(), name='performances-list'),
-    url(r'^api/performances/(?P<pk>\d+)/$', APIPerformancesDetail.as_view(), name='performances-detail'),
-]
+    url(r'^user/(\w+)/$', userpage),
+    url(r'^$', mainpage, name='home'),
+    url(r'^login/$', 'django.contrib.auth.views.login',name='login'),
+    url(r'^icinema/', include('icinema.urls', namespace='icinema')),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout', kwargs={'next_page': '/'}),
+    url(r'^admin/', include(admin.site.urls)),
 
-
-urlpatterns = format_suffix_patterns(urlpatterns, allowed=['api', 'json', 'xml'])
+)
